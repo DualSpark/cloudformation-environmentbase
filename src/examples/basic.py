9e819b05cc18f4d959fb69d105a1b2c63f752033
Eric Price <eric@dualspark.com>
2015-07-31 16:51:12 -0700

    Show config_handler in basic example

--- src/examples/basic.py
+++ src/examples/basic.py
@@ -6,6 +6,13 @@ class MyEnvClass(NetworkBase):
     Class creates a VPC and common network components for the environment
     """
 
+    def __init__(self, *args, **kwargs):
+
+        # Add config handlers here if using template patterns requiring configuration
+        # self.add_config_handler(<template class>)
+
+        super(MyEnvClass, self).__init__(*args, **kwargs)
+
     def create_action(self):
 
         self.initialize_template()

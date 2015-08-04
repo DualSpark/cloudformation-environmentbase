f0aa5e91098430022a93cbd1bfb05190298d49a6
Osman Dandia <odandia@gmail.com>
2015-08-04 14:39:15 -0700

    HaNat pattern: Change asg_size argument to asg_min and asg_max

--- src/environmentbase/patterns/ha_nat.py
+++ src/environmentbase/patterns/ha_nat.py
@@ -9,14 +9,15 @@ class HaNat(Template):
     Adds a highly available NAT that also serves as an NTP server
     '''
 
-    def __init__(self, asg_size=1, instance_type='t2.micro', install_ntp=False):
+    def __init__(self, asg_min=1, asg_max=1, instance_type='t2.micro', install_ntp=False):
         '''
         Method initializes HA NAT in a given environment deployment
         @param asg_size [number] - Number of instances in the autoscaling group
         @param instance_type [string] - Type of instances in the autoscaling group
         @param install_ntp [boolean] - Toggle for installing NTP on the NAT instances
         '''
-        self.asg_size = asg_size
+        self.asg_min = asg_min
+        self.asg_max = asg_max
         self.instance_type = instance_type
         self.install_ntp = install_ntp
 

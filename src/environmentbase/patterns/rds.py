ee3bd1a09b54c04642465317af9eea30eeca220e
Eric Price <eric@dualspark.com>
2015-08-07 17:22:39 -0700

    Moved rds example into rds pattern module

--- src/environmentbase/patterns/rds.py
+++ src/environmentbase/patterns/rds.py
@@ -1,5 +1,6 @@
 from environmentbase.template import Template
 import environmentbase.resources as res
+from environmentbase.networkbase import NetworkBase
 from troposphere import Ref, Parameter, GetAtt, rds
 
 
@@ -158,3 +159,52 @@ class RDS(Template):
                 'masteruser': Ref(admin_rds_user_name),
                 'masterpassword': Ref(admin_rds_password),
                 'securitygroups': self.security_groups}
+
+class Controller(NetworkBase):
+    """
+    Example class showing how to use the RDS pattern file to generate an RDS database as a child stack
+
+    To connect to this database log into the bastion and run:
+    > sudo yum install -y mysql
+    > mysql -h <db endpoint> -P <db port> -u <db username> -p
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.add_config_handler(rds.RDS)
+        super(Controller, self).__init__(*args, **kwargs)
+
+    def create_action(self):
+        self.initialize_template()
+        self.construct_network()
+
+        # Supply the user password as a manual parameter binding
+        self.manual_parameter_bindings['mydbRdsMasterUserPassword'] = 'secret123password'
+
+        # Create the rds instance pattern (includes standard standard parameters)
+        my_db = RDS(
+            security_groups=[],
+            subnet_set='private',
+            rds_args=self.config['db']['mydb'])
+
+        # Attach pattern as a child template
+        self.add_child_template(my_db)
+
+        # After attaching the db as a child template you can access the created resources
+        # {
+        #     'rds': DBInstance
+        #     'endpoint_address':GetAtt
+        #     'masterpassword': Ref
+        #     'securitygroups': [SecurityGroups],
+        #     'masteruser': Ref
+        #     'endpoint_port': GetAtt
+        #     'dbname': Ref
+        # }
+        print my_db.data
+
+        # Our template is complete output it to file
+        self.write_template_to_file()
+
+
+if __name__ == '__main__':
+
+    Controller()

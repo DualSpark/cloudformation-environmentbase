7a2ccc8cd9547b79d97bab564a385a4086247254
Osman Dandia <odandia@gmail.com>
2015-10-14 15:41:09 -0700

    Add support for both internal and internet-facing ELB in HaCluster pattern

--- src/environmentbase/patterns/ha_cluster.py
+++ src/environmentbase/patterns/ha_cluster.py
@@ -2,11 +2,15 @@ from environmentbase.template import Template
 from environmentbase import resources
 from troposphere import Ref, Base64, Join, Output, GetAtt, ec2
 
+SCHEME_INTERNET_FACING = 'internet-facing'
+SCHEME_INTERNAL = 'internal'
+PUBLIC_ACCESS_CIDR = '0.0.0.0/0'
+
 
 class HaCluster(Template):
     """
     Generic highly available cluster template
-    Contains an ELB, Autoscaling Group, Security Groups, and optional internal DNS name
+    Contains an ELB, Autoscaling Group, Security Groups, and optional internal DNS record
     """
 
     def __init__(self, 
@@ -15,7 +19,8 @@ class HaCluster(Template):
                  elb_ports={80: 80}, 
                  user_data_file='', 
                  min_size=1, max_size=1,
-                 subnet_layer='private'):
+                 subnet_layer='private',
+                 elb_scheme=SCHEME_INTERNET_FACING):
 
         # This will be the name used in resource names and descriptions
         self.name = name
@@ -36,8 +41,8 @@ class HaCluster(Template):
         # This is the subnet layer that the ASG is in (public, private, ...)
         self.subnet_layer = subnet_layer
 
-        # This is the type of the ELB (Internal or External)
-        # TODO: Implement this toggle in template.add_elb
+        # This is the type of ELB: internet-facing gets a publicly accessible DNS, while internal is only accessible to the VPC
+        self.elb_scheme = elb_scheme
 
         # This is an optional DNS entry to create a CNAME in a private hosted zone
         # TODO: Use template.register_elb_to_dns
@@ -50,15 +55,20 @@ class HaCluster(Template):
         Hook to add tier-specific assets within the build stage of initializing this class.
         """
 
-        # This creates security groups for the ASG, the ELB, and connects them together
+        # Create security groups for the ASG and ELB and connect them together
         security_groups = self.add_security_groups()
 
-        # This creates the ELB, opens the specified ports, and attachs the security group and logging bucket
+        # Determine the subnet layer of the ELB based on the scheme -- public if it's internet facing, else use the same subnet layer as the ASG
+        elb_subnet_layer = 'public' if self.elb_scheme == SCHEME_INTERNET_FACING else self.subnet_layer
+
+        # This creates the ELB, opens the specified ports, and attaches the security group and logging bucket
         ha_cluster_elb = self.add_elb(
             resource_name=self.name,
             security_groups=[security_groups['elb']],
             ports=self.elb_ports,
-            utility_bucket=self.utility_bucket
+            utility_bucket=self.utility_bucket,
+            subnet_layer=elb_subnet_layer,
+            scheme=self.elb_scheme
         )
 
         # This loads the userdata file from the data directory to be loaded into the ASG's launch configuration
@@ -90,10 +100,13 @@ class HaCluster(Template):
         Wrapper method to encapsulate process of creating security groups for this tier.
         """
 
+        # Determine ingress rules for ELB security -- open to internet for internet-facing ELB, open to VPC for internal ELB
+        access_cidr = PUBLIC_ACCESS_CIDR if self.elb_scheme == SCHEME_INTERNET_FACING else self.vpc_cidr
+
         # Create the ingress rules to the ELB security group
         elb_sg_ingress_rules = []
         for elb_port in self.elb_ports:
-            elb_sg_ingress_rules.append(ec2.SecurityGroupRule(FromPort=elb_port, ToPort=elb_port, IpProtocol='tcp', CidrIp=self.vpc_cidr))
+            elb_sg_ingress_rules.append(ec2.SecurityGroupRule(FromPort=elb_port, ToPort=elb_port, IpProtocol='tcp', CidrIp=access_cidr))
 
         # Create the ELB security group and attach the ingress rules
         elb_sg_name = '%sElbSecurityGroup' % self.name

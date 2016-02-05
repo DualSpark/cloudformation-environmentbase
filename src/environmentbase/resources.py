dc63b489ba5fb3eb3477965cf7b2efe783cec7ec
Eric Price <eric@dualspark.com>
2016-02-05 09:48:05 -0800

    Cleanup of resources.py w new tests

--- src/environmentbase/resources.py
+++ src/environmentbase/resources.py
@@ -1,138 +1,361 @@
 from pkg_resources import resource_string, resource_exists
-import yaml, json
+import copy
+import yaml
+import json
 import os
+import re
 
 
-def _test_filelike(parent, basename, validator):
-    """
-    Tests various file extension to find the requested file like resource
-    :param parent: parent directory
-    :param basename: portion of filename excluding the file extension
-    :return: file path of the resource that exists, or None
-    """
-    (basename, provided_suffix) = os.path.splitext(basename)
-    suffix_list = EXTENSIONS
-    if provided_suffix == '' or provided_suffix not in suffix_list:
-        suffix_list = [provided_suffix] + EXTENSIONS
-
-    for extension in suffix_list:
-        file_path = os.path.join(parent, basename + extension)
-        if validator(file_path):
-            return file_path
-
-    return None
+# Declare R to be the singleton Resource instance
+R = None
 
 
-def test_resource(parent, basename, relative_to_module_name=__name__):
-    resource_test = lambda file_path: resource_exists(relative_to_module_name, file_path)
-    return _test_filelike(parent, basename, resource_test)
+class Res(object):
 
+    CONFIG_FILENAME = "config.json"
+    IMAGE_MAP_FILENAME = "ami_cache.json"
+    INSTANCETYPE_MAP_FILENAME = "instancetype_to_arch.json"
 
-def test_file(parent, basename):
-    file_test = lambda file_path: os.path.isfile(file_path)
-    return _test_filelike(parent, basename, file_test)
+    DEFAULT_DATA_PATH = "data"
 
+    # Configure resource loading for yaml parser (i.e. yaml.load())
+    _INCLUDE_RESOURCE_MODULE = __name__
+    _INCLUDE_RESOURCE_INTERNAL_PATH = DEFAULT_DATA_PATH
 
-def get_yaml_resource(resource_name, relative_to_module_name=__name__):
-    """
-    Get package resource as json
-    """
-    return yaml.load(get_resource(resource_name, relative_to_module_name))
-
-
-def get_resource(resource_name, relative_to_module_name=__name__):
-    """
-    Retrieves resource embedded in the package (even if installed as a zipped archive).
-    """
-    file_path = test_resource('data', resource_name, relative_to_module_name)
-    file_content = resource_string(relative_to_module_name, file_path)
-    return file_content
-
-
-# Implimentation of "!include" directive for yaml parser to load YAML content from external file
-def yaml_include(loader, node):
-    # Get the path out of the yaml file
-    file_name = os.path.join(os.path.dirname(loader.name), node.value)
-
-    with file(file_name) as inputfile:
-        return yaml.load(inputfile)
-
-
-# Modify yaml parser to support "!include" directive to embed content from an external files
-yaml.add_constructor("!include", yaml_include)
-
-EXTENSIONS = ['.json', '.yaml', '.yml']
-
-DEFAULT_CONFIG_FILENAME = 'config'
-FACTORY_DEFAULT_CONFIG = get_yaml_resource(DEFAULT_CONFIG_FILENAME)
-FACTORY_DEFAULT_CONFIG_STRING = get_resource(DEFAULT_CONFIG_FILENAME)
-
-DEFAULT_AMI_CACHE_FILENAME = 'ami_cache'
-FACTORY_DEFAULT_AMI_CACHE = get_yaml_resource(DEFAULT_AMI_CACHE_FILENAME)
-
-CONFIG_REQUIREMENTS_FILENAME = 'config_schema'
-CONFIG_REQUIREMENTS = get_yaml_resource(CONFIG_REQUIREMENTS_FILENAME)
-
-
-COMMON_STRINGS_FILENAME = 'common_strings'
-COMMON_STRINGS = get_yaml_resource(COMMON_STRINGS_FILENAME)
-
-
-def load_file(parent, basename):
-    file_path = test_file(parent, basename)
-    if not file_path:
-        raise Exception("%s does not exist. Try running the init command to generate it.\n" % (basename + EXTENSIONS[0]))
-
-    return load_yaml_file(file_path)
-
-def load_yaml_file(file_path):
-
-    if not os.path.isfile(file_path):
-        raise Exception('{} does not exist'.format(file_path))
-
-    with open(file_path, 'r') as f:
-        try:
-            content = f.read()
-            parsed_content = yaml.load(content)
-        except ValueError:
-            print '%s could not be parsed' % file_path
-            raise
-
-    return parsed_content
-
-def load_json_file(file_path):
-
-    if not os.path.isfile(file_path):
-        raise Exception('{} does not exist'.format(file_path))
-
-    with open(file_path, 'r') as f:
-        try:
-            content = f.read()
-            parsed_content = json.loads(content)
-        except ValueError:
-            print '%s could not be parsed' % file_path
-            raise
-
-    return parsed_content
-
-
-def get_str(key, default=None):
-    return COMMON_STRINGS.get(key, default)
-
-
-def get_type(typename):
-    """
-    Convert typename to type object
-    :param typename: String name of type
-    :return: __builtin__ type instance
-    """
-    types = {
-        'bool': bool,
-        'int': int,
-        'float': float,
-        # avoid all the python unicode weirdness by making all the strings basestrings
-        'str': basestring,
-        'basestring': basestring,
-        'list': list
+    # Generated config sections to break out to separate files
+    _EXTRACTED_CONFIG_SECTIONS = {
+        'image_map': IMAGE_MAP_FILENAME,
+        'instancetype_to_arch': INSTANCETYPE_MAP_FILENAME
     }
-    return types.get(typename, None)
+
+    # Resource cache, prevents multiple loads of the same file
+    _loaded_files = {}
+
+    def __init__(self):
+        # Set PyYAML's '!include' constructor to use the file loader
+        # Any function that changes this should set it back before exiting
+        yaml.add_constructor("!include", Res.yaml_file_include)
+
+    # Implimentation of "!include" directive for yaml parser to load YAML content from egg archive resource
+    @staticmethod
+    def yaml_resource_include(loader, node):
+        content = R.load_resource(
+            node.value,
+            module=Res._INCLUDE_RESOURCE_MODULE,
+            internal_path=Res._INCLUDE_RESOURCE_INTERNAL_PATH)
+        return yaml.load(content)
+
+    # Implimentation of "!include" directive for yaml parser to load YAML content from filesystem resource
+    @staticmethod
+    def yaml_file_include(loader, node):
+        # Get the path out of the yaml file
+        file_name = os.path.join(os.path.dirname(loader.name), node.value)
+        if os.path.isfile(file_name):
+            with file(file_name) as inputfile:
+                return yaml.load(inputfile)
+        else:
+            raise Exception("Could not load file '%s'" % node.value)
+
+    def load_resource(self,
+                      filename,
+                      module=None,
+                      internal_path=None):
+        """
+        @param filename [string] The name of the file withn the egg archive
+        @param module [string] A module name within the egg archive, 'internal_path'
+        must be sibling to this location within the archive. Typically magic var '__name__'.
+        @param internal_path [string] File path prepended to filename e.g. <internal_path>/<filename>
+        Return content of a resource embedded within an egg archive.
+        """
+
+        # Can't set with param vaules for some reason
+        if not module:
+            module = Res._INCLUDE_RESOURCE_MODULE
+        if not internal_path:
+            internal_path = Res._INCLUDE_RESOURCE_INTERNAL_PATH
+
+        # Attempt to retreive cached content
+        key = "%s:%s:%s" % (module, internal_path, filename)
+        if key in self._loaded_files:
+            return self._loaded_files[key]
+
+        filepath = os.path.join(internal_path, filename)
+
+        if not resource_exists(module, filepath):
+            raise Exception("Resource '%s' not found in module '%s'" % (filename, module))
+
+        file_content = resource_string(module, filepath)
+
+        # cache file_content
+        self._loaded_files[key] = file_content
+
+        return file_content
+
+    # def gather_includes(self, filename, module=None, internal_path=None):
+    #     """
+    #     Scan file from resource path for '!include' resources.  Return list of all referenced files.
+    #     This method will fail if a file is referenced more than once.
+    #     """
+
+    #     # Can't set with param vaules for some reason
+    #     if not module:
+    #         module = Res._INCLUDE_RESOURCE_MODULE
+    #     if not internal_path:
+    #         internal_path = Res._INCLUDE_RESOURCE_INTERNAL_PATH
+
+    #     processed_includes = []
+    #     pending_includes = [filename]
+
+    #     while pending_includes:
+    #         current = pending_includes.pop()
+    #         content = self.load_resource(current, module, internal_path)
+
+    #         for m in re.finditer(r"!include ([a-zA-Z0-9_.\\-]*)", content):
+    #             file_ref = m.group(1)
+
+    #             if file_ref in processed_includes:
+    #                 raise Exception("Repeat reference for '%s' in file %s" % (file_ref, filename))
+
+    #             pending_includes.append(file_ref)
+
+    #         processed_includes.append(current)
+
+    #     return processed_includes
+
+    # def copy_from_egg(self, filename, destination='.', module=None, internal_path=None):
+    #     """
+    #     Copy file from egg, including all dependent files referenced with '!include'
+    #     """
+
+    #     # Can't set with param vaules for some reason
+    #     if not module:
+    #         module = Res._INCLUDE_RESOURCE_MODULE
+    #     if not internal_path:
+    #         internal_path = Res._INCLUDE_RESOURCE_INTERNAL_PATH
+
+    #     filenames = self.gather_includes(filename, module, internal_path)
+
+    #     for current_filename in filenames:
+    #         # Load the cached resource
+    #         content = self.load_resource(current_filename, module, internal_path)
+
+    #         # Write file content to local file
+    #         filepath = os.path.join(destination, current_filename)
+    #         with open(filepath, 'w') as f:
+    #             f.write(content)
+
+    def parse_file(self, filename, from_file=True):
+        """
+        Read file into python data structure from either EGG archive or local filesystem.
+        Note: File may contain !include references to other files relative to the requested file.
+        @param filename [string] Name of file to load.
+        @param from_file [boolean] If true loades files from fs otherwise file loaded from resource
+        path using _INCLUDE_RESOURCE_MODULE and _INCLUDE_RESOURCE_INTERNAL_PATH.
+        """
+        # Load file content from file or resource path
+        if from_file:
+            with file(filename) as f:
+                content = f.read()
+        else:
+            content = self.load_resource(
+                filename,
+                module=Res._INCLUDE_RESOURCE_MODULE,
+                internal_path=Res._INCLUDE_RESOURCE_INTERNAL_PATH)
+
+        # Configure PyYAML to process '!include' directive with correct handler function
+        if not from_file:
+            yaml.add_constructor("!include", Res.yaml_resource_include)
+
+        # parse and return
+        parsed_content = yaml.load(content)
+
+        # Set PyYAML's !include back to loading from files
+        if not from_file:
+            yaml.add_constructor("!include", Res.yaml_file_include)
+
+        return parsed_content
+
+    def extract_config_section(self, config, config_key, filename):
+        """
+        Write requested config section to file and replace config value with a sentinel value to
+        be processed later into a valid '!include' directive. The sentinel is a string containing
+        the correct include directive.
+        @parse config [list|dict] The config datastructure to be modified with a template token.
+        @param config_key [string] The config key to be externalized.
+        @param filename [string] The name of the file created to hold config[config_key]
+        """
+        section = config.get(config_key)
+
+        # Output file
+        with open(filename, 'w') as f:
+            content = json.dumps(section, indent=4, separators=(',', ': '))
+            f.write(content)
+            print "Generated %s file at %s\n" % (config_key, filename)
+
+        config[config_key] = "!include %s" % filename
+
+    def generate_config(self, config_file=CONFIG_FILENAME, config_handlers=list(), extract_map=_EXTRACTED_CONFIG_SECTIONS):
+        """
+
+        """
+        # Load config from egg
+        config = self.parse_file(config_file, from_file=False)
+
+        # Merge in any defaults provided by registered config handlers
+        for handler in config_handlers:
+            config.update(handler.get_factory_defaults())
+
+        # Make changes to a new copy of the config
+        config_copy = copy.deepcopy(config)
+
+        # Since the !include references are not standard json we need to use special values we can
+        # find and replace after serializing to string.
+
+        # Write config sections to file and replace content with "!include" string.
+        for section_key, filename in extract_map.iteritems():
+            self.extract_config_section(config_copy, section_key, filename)
+
+        # Serialize config to string
+        templatized_config_string = json.dumps(config_copy, indent=4, separators=(',', ': '))
+
+        # Replace encoded 'include' with the real one using regex.
+        # This amounts to capturing the quoted string and stripping off the quotes
+        final_config_string = re.sub(r"\"!include ([a-zA-Z0-9_.\\-]*)\"",
+                                     lambda m: m.group(0)[1:-1],
+                                     templatized_config_string)
+
+        # Finally write config.json to file
+        with open(config_file, 'w') as f:
+            f.write(final_config_string)
+            print "Generated config file at %s\n" % 'config.json'
+
+        return final_config_string
+
+
+# Assign singleton Resource instance now that the class is defined
+R = Res()
+
+
+
+# def _test_filelike(parent, basename, validator):
+#     """
+#     Tests various file extension to find the requested file like resource
+#     :param parent: parent directory
+#     :param basename: portion of filename excluding the file extension
+#     :return: file path of the resource that exists, or None
+#     """
+#     (basename, provided_suffix) = os.path.splitext(basename)
+#     suffix_list = EXTENSIONS
+#     if provided_suffix == '' or provided_suffix not in suffix_list:
+#         suffix_list = [provided_suffix] + EXTENSIONS
+
+#     for extension in suffix_list:
+#         file_path = os.path.join(parent, basename + extension)
+#         if validator(file_path):
+#             return file_path
+
+#     return None
+
+
+# def test_resource(parent, basename, relative_to_module_name=__name__):
+#     resource_test = lambda file_path: resource_exists(relative_to_module_name, file_path)
+#     return _test_filelike(parent, basename, resource_test)
+
+
+# def test_file(parent, basename):
+#     file_test = lambda file_path: os.path.isfile(file_path)
+#     return _test_filelike(parent, basename, file_test)
+
+
+# def get_yaml_resource(resource_name, relative_to_module_name=__name__):
+#     """
+#     Get package resource as json
+#     """
+#     return yaml.load(get_resource(resource_name, relative_to_module_name))
+
+
+# def get_resource(resource_name, relative_to_module_name=__name__):
+#     """
+#     Retrieves resource embedded in the package (even if installed as a zipped archive).
+#     """
+#     print "******* relative_to_module_name = " + relative_to_module_name
+#     file_path = test_resource('data', resource_name, relative_to_module_name)
+#     file_content = resource_string(relative_to_module_name, file_path)
+#     return file_content
+
+
+# EXTENSIONS = ['.json', '.yaml', '.yml']
+
+# DEFAULT_CONFIG_FILENAME = 'config'
+# FACTORY_DEFAULT_CONFIG_STRING = get_resource(DEFAULT_CONFIG_FILENAME)
+# FACTORY_DEFAULT_CONFIG = yaml.load(FACTORY_DEFAULT_CONFIG_STRING)
+
+# DEFAULT_AMI_CACHE_FILENAME = 'ami_cache'
+# FACTORY_DEFAULT_AMI_CACHE = get_yaml_resource(DEFAULT_AMI_CACHE_FILENAME)
+
+# CONFIG_REQUIREMENTS_FILENAME = 'config_schema'
+# CONFIG_REQUIREMENTS = get_yaml_resource(CONFIG_REQUIREMENTS_FILENAME)
+
+
+# COMMON_STRINGS_FILENAME = 'common_strings'
+# COMMON_STRINGS = get_yaml_resource(COMMON_STRINGS_FILENAME)
+
+
+# def load_file(parent, basename):
+#     file_path = test_file(parent, basename)
+#     if not file_path:
+#         raise Exception("%s does not exist. Try running the init command to generate it.\n" % (basename + EXTENSIONS[0]))
+
+#     return load_yaml_file(file_path)
+
+# def load_yaml_file(file_path):
+
+#     if not os.path.isfile(file_path):
+#         raise Exception('{} does not exist'.format(file_path))
+
+#     with open(file_path, 'r') as f:
+#         try:
+#             content = f.read()
+#             parsed_content = yaml.load(content)
+#         except ValueError:
+#             print '%s could not be parsed' % file_path
+#             raise
+
+#     return parsed_content
+
+# def load_json_file(file_path):
+
+#     if not os.path.isfile(file_path):
+#         raise Exception('{} does not exist'.format(file_path))
+
+#     with open(file_path, 'r') as f:
+#         try:
+#             content = f.read()
+#             parsed_content = json.loads(content)
+#         except ValueError:
+#             print '%s could not be parsed' % file_path
+#             raise
+
+#     return parsed_content
+
+
+# def get_str(key, default=None):
+#     return COMMON_STRINGS.get(key, default)
+
+
+# def get_type(typename):
+#     """
+#     Convert typename to type object
+#     :param typename: String name of type
+#     :return: __builtin__ type instance
+#     """
+#     types = {
+#         'bool': bool,
+#         'int': int,
+#         'float': float,
+#         # avoid all the python unicode weirdness by making all the strings basestrings
+#         'str': basestring,
+#         'basestring': basestring,
+#         'list': list
+#     }
+#     return types.get(typename, None)
---
+++ src/tests/test_resources.py
@@ -0,0 +1,168 @@
+from unittest2 import TestCase
+from environmentbase.resources import Res
+from tempfile import mkdtemp
+import shutil
+import os
+import json
+import yaml
+
+
+class ResourcesTestCase(TestCase):
+
+    def setUp(self):
+        Res._INCLUDE_RESOURCE_MODULE = 'environmentbase.resources'
+        Res._INCLUDE_RESOURCE_INTERNAL_PATH = 'data'
+
+        self.r = Res()
+
+        # Change to a temp dir so auto generated file don't clutter the os
+        self.temp_dir = mkdtemp()
+        os.chdir(self.temp_dir)
+
+    def tearDown(self):
+        # Delete any files left in the temp dir
+        shutil.rmtree(self.temp_dir)
+        assert not os.path.isdir(self.temp_dir)
+
+    def test_load_resource(self):
+        # validate load from usual location
+        content = self.r.load_resource('config.json')
+        self.assertIn('global', content)
+
+        # validate load from custom location
+        content = self.r.load_resource('amzn_linux_ec2.json', module=__name__, internal_path='resources')
+        self.assertIn("Sample Template", content)
+
+        # Validate failure scenerios
+        with self.assertRaises(Exception):
+            self.r.load_resource('fake_file')
+
+        with self.assertRaises(Exception):
+            self.r.load_resource('config.json', resource_dir='wrong_path')
+
+        with self.assertRaises(Exception):
+            self.r.load_resource('config.json', module="wrong_module")
+
+    def test_parse_file(self):
+
+        # Configure resource loading to read from the test module
+        Res._INCLUDE_RESOURCE_MODULE = __name__
+        Res._INCLUDE_RESOURCE_INTERNAL_PATH = 'resources'
+
+        # Verify resource loading:
+        parsed_content = self.r.parse_file('amzn_linux_ec2.json', from_file=False)
+        self.assertIn("Sample Template", parsed_content['Description'])
+
+        # Save this file to the current directory (the temp dir for this test run)
+        # with a modified description for verification
+        parsed_content['Description'] = 'Blaaa'
+        with open('amzn_linux_ec2.json', 'w') as f:
+            f.write(json.dumps(parsed_content, indent=4, separators=(',', ': ')))
+
+        # Verify file loading
+        parsed_content = self.r.parse_file('amzn_linux_ec2.json', from_file=True)
+        self.assertEquals("Blaaa", parsed_content['Description'])
+
+    def test_extract_config_section(self):
+        # Create bogus config object
+        config = {
+            'a': 'don\'t extract',
+            'b': {'map': 'extract', 'test': range(1, 10)}
+        }
+
+        # verify extraction of complex structure
+        self.r.extract_config_section(config, 'b', 'b.json')
+        self.assertEquals(config['b'], '!include b.json')
+        with file('b.json') as f:
+            content = f.read()
+            parsed_content = yaml.load(content)
+            self.assertEquals(parsed_content, {'map': 'extract', 'test': range(1, 10)})
+
+        # verify unextracted section was not changed
+        self.assertEquals(config['a'], 'don\'t extract')
+
+    def test_generate_config(self):
+        # Configure resource loading to read from the test module
+        Res._INCLUDE_RESOURCE_MODULE = __name__
+        Res._INCLUDE_RESOURCE_INTERNAL_PATH = 'resources'
+
+        custom_config_addition = {
+            'custom': {
+                'a': 'don\'t extract',
+                'b': {'map': 'extract', 'test': range(1, 10)}
+            },
+            "AWSTemplateFormatVersion": "2016-02-05"
+        }
+
+        custom_config_validation = {
+            'custom': {
+                'a': 'basestring',
+                'b': {'map': 'basestring', 'test': 'list'}
+            }
+        }
+
+        class CustomHandler(object):
+            @staticmethod
+            def get_factory_defaults():
+                return custom_config_addition
+
+            @staticmethod
+            def get_config_schema():
+                return custom_config_validation
+
+        # Treat sample cfn template as if it were a config file for testing purposes
+        self.r.generate_config(
+            config_file='amzn_linux_ec2.json',
+            extract_map={
+                "Description": "description.json",
+                "Parameters": "params.json",
+                "Mappings": "mappings.json",
+                "Resources": "resources.json",
+                "Outputs": "output.json"
+            },
+            config_handlers=[CustomHandler()]
+        )
+
+        # Make sure all the extracted files exist
+        with file('amzn_linux_ec2.json') as f:
+            content = f.read()
+            for inc_file in ['description.json', 'params.json', 'mappings.json', 'resources.json', 'output.json']:
+                self.assertIn('!include %s' % inc_file, content)
+
+        # Read the JSON into Python data structure
+        parsed_content = self.r.parse_file('amzn_linux_ec2.json', from_file=True)
+
+        # Verify you got a datastructure back
+        self.assertTrue(isinstance(parsed_content, dict))
+
+        # Verify new config section is added
+        self.assertEquals(parsed_content['custom'], custom_config_addition['custom'])
+
+        # Verify modified config section is modified
+        self.assertEquals(parsed_content['AWSTemplateFormatVersion'], "2016-02-05")
+
+# def test_gather_includes(self):
+    #     files = r.gather_includes('config_tree_root', module=__name__, internal_path='resources/includes')
+    #     self.assertEquals(len(files), 4)
+
+    #     with self.assertRaises(Exception):
+    #         r.gather_includes('config_self_ref', module=__name__, internal_path='resources/includes')
+
+    #     with self.assertRaises(Exception):
+    #         r.gather_includes('circular_ref_a', module=__name__, internal_path='resources/includes')
+
+    # def test_copy_from_egg(self):
+    #     resources = ['config_tree_root', 'config_tree_left', 'config_tree_left_left', 'config_tree_right']
+
+    #     # current directory test
+    #     r.copy_from_egg('config_tree_root', module=__name__, internal_path='resources/includes')
+
+    #     for resource_name in resources:
+    #         self.assertTrue(os.path.isfile(resource_name))
+
+    #     # destination test
+    #     new_dir = mkdtemp()
+    #     r.copy_from_egg('config_tree_root', destination=new_dir, module=__name__, internal_path='resources/includes')
+    #     for resource_name in resources:
+    #         new_path = os.path.join(new_dir, resource_name)
+    #         self.assertTrue(os.path.isfile(new_path))

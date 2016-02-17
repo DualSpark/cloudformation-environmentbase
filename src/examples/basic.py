16a2734b4bdda996f4ee5e6f3c1b91e495d138f6
Eric Price <eric@dualspark.com>
2016-02-17 12:48:28 -0800

    Complete removal of network config from data/config.json

--- src/examples/basic.py
+++ src/examples/basic.py
@@ -1,6 +1,8 @@
+from environmentbase.environmentbase import EnvConfig
 from environmentbase.networkbase import NetworkBase
 from environmentbase.patterns.bastion import Bastion
 from environmentbase.patterns.ha_cluster import HaCluster
+from environmentbase.patterns.base_network import BaseNetwork
 
 
 class MyEnvClass(NetworkBase):
@@ -24,4 +26,5 @@ class MyEnvClass(NetworkBase):
             suggested_instance_types=['t2.micro']))
 
 if __name__ == '__main__':
-    MyEnvClass()
+    env_config = EnvConfig(config_handlers=[BaseNetwork])
+    MyEnvClass(env_config=env_config)
